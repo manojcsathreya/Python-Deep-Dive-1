19023abe042149f6962c3d8537603c6a534d4d91
manojcsathreya <39308808+manojcsathreya@users.noreply.github.com>
2021-12-19 10:06:55 +0530

    Update 2,3: identifiers and conditional statements.py

--- 2,3: identifiers and conditional statements.py
+++ 2,3: identifiers and conditional statements.py
@@ -18,3 +18,101 @@ Variables : lowercase, words separated by underscores (snake_case) : Example-acc
 Constants : all-uppercase, words separated by underscores : Example-MIN_APR
 '''
 
+#3. Conditional statements
+
+a=5
+if a <5:
+    print('a<5')
+else:
+    if a<10:
+        print('5<=a<10')
+    else:
+        print('a>=10')
+   #op: 5<=a<10      
+
+a=10
+if a <5:
+    print('a<5')
+else:
+    if a<10:
+        print('5<=a<10')
+    else:
+        print('a>=10')
+        
+        #op: a>=10
+
+a=6
+if a <5:
+    print('a<5')
+else:
+    if a<10:
+        print('5<=a<10')
+    else:
+        print('a>=10')
+        
+        #op: 5<=a<10
+        
+        
+# we do not have switch statements in python
+#instaed we use elif
+
+a = 4
+
+if a<5:
+    print('a<5')
+elif a<10:
+    print('5<=a<10')
+elif a<15:
+    print('10<=a<15')
+elif a<20:
+    print('15<=a<20')
+else:
+    print(a>=20)
+#op:a<5
+
+a = 6
+
+if a<5:
+    print('a<5')
+elif a<10:
+    print('5<=a<10')
+elif a<15:
+    print('10<=a<15')
+elif a<20:
+    print('15<=a<20')
+else:
+    print(a>=20)
+#op:<=a<10
+
+a = 10
+
+if a<5:
+    print('a<5')
+elif a<10:
+    print('5<=a<10')
+elif a<15:
+    print('10<=a<15')
+elif a<20:
+    print('15<=a<20')
+else:
+    print(a>=20)
+#op:10<=a<15
+
+a = 20
+
+if a<5:
+    print('a<5')
+elif a<10:
+    print('5<=a<10')
+elif a<15:
+    print('10<=a<15')
+elif a<20:
+    print('15<=a<20')
+else:
+    print('a>=20')
+#op:a>=20
+
+#we also have ternary operator
+#syntax return a if true else return b
+a=4
+print('a<5' if a<5 else 'a>5')
