92b45283b2933fe9f79476b231471debd4f0f1c6
manojcsathreya <39308808+manojcsathreya@users.noreply.github.com>
2021-12-19 09:54:32 +0530

    Create 2,3: identifiers and conditional statements.py

---
+++ 2,3: identifiers and conditional statements.py
@@ -0,0 +1,20 @@
+#identifiers are case sensitive
+#must start with underscore or letters followed by underscore or letter or numbers
+#cannot be identifiers
+'''
+when an identifiers starts with single underscore: This is a convention followed to indicate that the identifier as an private variable
+when an identifiers starts with double underscore : This is a convention followed to indicate that the identifier is used in inheritance
+when an identifiers starts with double underscore and ends with double underscore: used for system defined, and has special meaning to interpreter(Don;t invent one. Just use the pre defined ones)
+'''
+'''
+Naming conventions prescribed in pep8
+Packages : short, all-lowercase names. Preferably no underscores. : example: utilities
+Modules: short, all-lowercase names. Can have underscores.
+Packages : short, all-lowercase names. Preferably no underscores. : Example-utilities
+db_utils dbutils
+Classes : CapWords (upper camel case) :Example- BankAccount
+Functions : lowercase, words separated by underscores (snake_case) :Example-open_account
+Variables : lowercase, words separated by underscores (snake_case) : Example-account_id
+Constants : all-uppercase, words separated by underscores : Example-MIN_APR
+'''
+
